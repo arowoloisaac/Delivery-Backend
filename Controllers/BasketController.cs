ed0621599f046119b9f0e63efdf52b7ea8bf15db
Isaac Arowolo <isaac.arowolo@csdevents.com>
2023-12-20 00:51:52 +0700

    Basket Controller
    Implemented the basket controller and add the main or major functionalities needed to perform action with it

---
+++ Controllers/BasketController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arowolo_Delivery_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetBasket()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("dish/{dishId}")]
+        public async Task<IActionResult> AddToCart(Guid dishId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("dish/{dishId}")]
+        public async Task<IActionResult> DeleteCart(Guid dishId, bool increase)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
