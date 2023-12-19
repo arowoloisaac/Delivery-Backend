e7cf6e81c0518c9d3020c8056098f0928f080a9c
Isaac Arowolo <isaac.arowolo@csdevents.com>
2023-12-19 16:28:55 +0700

    User Service
    Implemented the function for viewing Profile and editing user profile from the dbcontext. For the logging in user and validating user that checks if the user email alaedy exist, I added a function (generatetoken) that aids user with login

--- Services/UserService/UserService.cs
+++ Services/UserService/UserService.cs
@@ -1,31 +1,82 @@
-﻿using Arowolo_Delivery_Project.Dtos.UserDtos;
+﻿using Arowolo_Delivery_Project.Cofiguration;
+using Arowolo_Delivery_Project.Dtos.UserDtos;
 using Arowolo_Delivery_Project.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Arowolo_Delivery_Project.Services.UserService
 {
     public class UserService : IUserService
     {
         private readonly UserManager<User> _userManager;
+        private readonly JwtBearerTokenSettings _bearerTokenSettings;
 
-        public UserService(UserManager<User> userManger)
+        public UserService(UserManager<User> userManger, IOptions<JwtBearerTokenSettings> jwtTokenOptions)
         {
             _userManager = userManger;
+            _bearerTokenSettings = jwtTokenOptions.Value;
         }
 
-        public Task EditProfile(EditUserDto request)
+        public async Task EditProfile(EditUserDto request)
         {
-            throw new NotImplementedException();
+            var existingUser = await _userManager.Users.FirstOrDefaultAsync( x => x.FullName.ToLower() == request.FullName.ToLower() );
+
+            if (existingUser is not null)
+            {
+                existingUser.Address = request.Address;
+                existingUser.BirthDate = request.BirthDate;
+                existingUser.PhoneNumber = request.PhoneNumber;
+                existingUser.Gender = request.Gender;
+                existingUser.FullName = request.FullName;
+
+                var updateUser = await _userManager.UpdateAsync(existingUser);
+
+                if (!updateUser.Succeeded)
+                {
+                    throw new Exception("Failed to update user profile");
+                }
+            }
+
+            throw new ArgumentNullException("No Active user");
         }
 
-        public Task<UserProfileDto> GetProfile(string email)
+        public async Task<UserProfileDto> GetProfile(string email)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                throw new KeyNotFoundException($"User with email {email} already exist");
+            }
+
+            return new UserProfileDto
+            {
+                Email = user.Email,
+                BirthDate = user.BirthDate,
+                PhoneNumber = user.PhoneNumber,
+                Name = user.FullName,
+                Address = user.Address,
+                Gender = user.Gender,
+                Id = user.Id,
+            };
         }
 
-        public Task<string> Login(LoginUserDto request)
+        public async Task<string> Login(LoginUserDto request)
         {
-            throw new NotImplementedException();
+            var user = await ValidateUser(request);
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("Login Failed");
+            }
+
+            var role = await _userManager.IsInRoleAsync(user, ApplicationRoleNames.Administrator);
+            return GenerateToken(user, role);
         }
 
         public async Task Register(RegisterUserDto request)
@@ -53,6 +104,48 @@ namespace Arowolo_Delivery_Project.Services.UserService
             {
                 throw new Exception($"Failed to validate user {request.Email}");
             }
+        }
+
+
+
+        private async Task<User> ValidateUser(LoginUserDto request)
+        {
+            var identifyUser = await _userManager.FindByEmailAsync(request.Email);
+
+            if (identifyUser != null)
+            {
+                var result = _userManager.PasswordHasher.VerifyHashedPassword(identifyUser, identifyUser.PasswordHash, request.Password);
+
+                return result == PasswordVerificationResult.Success ? identifyUser : null;
+            }
+            return null;
+        }
+
+
+        private string GenerateToken(User user, bool isAdmin)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_bearerTokenSettings.SecretKey);
+
+            var descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Name, user.FullName),
+                    new Claim(ClaimTypes.Authentication, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, isAdmin ? ApplicationRoleNames.Administrator : ApplicationRoleNames.User)
+                }),
+                Expires = DateTime.UtcNow.AddSeconds(_bearerTokenSettings.ExpiryTimeInSeconds),
+                SigningCredentials =
+                    new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Audience = _bearerTokenSettings.Audience,
+                Issuer = _bearerTokenSettings.Issuer,
+            };
+
+            var token = tokenHandler.CreateToken(descriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
