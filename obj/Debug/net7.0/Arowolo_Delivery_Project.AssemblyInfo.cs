91626b5d140aad60284b3b53a6106e12dad472af
arowoloisaac <arowoloisaacabiodun01@gmail.com>
2024-10-29 20:29:49 +0700

    Here to check

--- obj/Debug/net7.0/Arowolo_Delivery_Project.AssemblyInfo.cs
+++ obj/Debug/net7.0/Arowolo_Delivery_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arowolo_Delivery_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae6700db6d901451c42883b6d9d8831f6f61f8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arowolo_Delivery_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arowolo_Delivery_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
